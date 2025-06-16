97ffa35fe602fe68661030637b47afad2a5e6473
jennymakki <jjennyeklundd@hotmail.com>
2025-06-16 13:06:49 +0200

    added header and footer components and styled them

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
