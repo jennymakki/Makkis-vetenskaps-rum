7adf8a28d8365e8d7de3136081bba22a8abb7062
jennymakki <jjennyeklundd@hotmail.com>
2025-12-01 17:27:25 +0100

    changed a title

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 app.set('view engine', 'ejs');
 
 app.get('/', (_req, res) => {
