98b5cfdef9c201dd6461a032676bc1aaa9269252
jennymakki <jjennyeklundd@hotmail.com>
2026-06-24 15:14:18 +0200

    updated header and migrated to tailwind

--- fares-labratorium-next/app/components/Header.tsx
+++ fares-labratorium-next/app/components/Header.tsx
@@ -1,21 +1,16 @@
-import Link from "next/link";
-
-import { Home } from "lucide-react";
 
 export default function Header() {
   return (
-    <header>
-      <div className="header__div">
-        <h1 className="header__h1">Makkis Vetenskapsrum</h1>
+<header className="bg-gradient-to-r from-[#14281D] via-[#203A2B] to-[#2F4F3E] text-white py-10 shadow-lg border-b border-white/10">
+      <div className="relative flex items-center justify-center max-w-6xl mx-auto px-4">
 
-        <Link
-          href="/"
-          className="header__icon-link"
-          aria-label="Go to homepage"
-        >
-          <Home className="header__icon" />
-        </Link>
-      </div>
-    </header>
+    <div className="text-center">
+      <h1 className="text-2xl md:text-5xl font-extralight tracking-[0.15em]">
+        Makkis Vetenskapsrum
+      </h1>
+    </div>
+
+  </div>
+</header>
   );
 }
